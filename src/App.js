f9238e17816543ec4de599a6e7a87a4d66fe6a89
Bablu Mia <bablumiah2000@gmail.com>
2022-04-28 23:59:30 +0600

    backend data is working

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom';
 import Login from './Components/Login/Login';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [datas,setDatas] =useState([])
+  useEffect(()=>{
+    fetch('http://localhost:5000/products')
+    .then(res=>res.json())
+    .then(data => setDatas(data))
+  },[])
   return (
     <div>
       {/* <h2>Hello From App</h2>
       <button className='btn btn-primary'>Hello</button> */}
+      {
+        datas.map(sdata => <h3>{sdata.model}</h3>)
+      }
       <Routes>
         <Route path='/login' element={<Login/>}></Route>
       </Routes>
