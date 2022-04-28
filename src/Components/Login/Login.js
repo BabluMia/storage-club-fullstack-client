f38c4a54414f5860c5e5fe412202a92c87fc9973
Bablu Mia <bablumiah2000@gmail.com>
2022-04-28 23:55:12 +0600

    file created

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,13 @@
+import "./Login.css";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <div >
+      
+    </div>
+  );
+};
+
+export default Login;
