7e1f465a0f47c921fe5755d319d73da35c2a50cf
Bablu Mia <bablumiah2000@gmail.com>
2022-04-30 11:33:25 +0600

    banner added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-    return (
-        <div>
-            {/* banner section */}
-            <div className="row">
-                <div className="col-12">
-                    
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      {/* banner section */}
+      <div className="container-fluid">
+          <div className="row">
+              <div className="col-12">
+                  <img src="http://demo.bestprestashoptheme.com/warehouse/modules/novnivoslider/images/7bf86086d5e35510fee4701490f352902deeb865_Untitled-1.png" className="img-fluid" alt="" />
+              </div>
+          </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
