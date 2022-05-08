fd2fa933352ac8c5a951227b5ed7c196dd0450fd
Bablu Mia <bablumiah2000@gmail.com>
2022-05-08 17:16:16 +0600

    final push

--- src/api/axiosPrivate.js
+++ src/api/axiosPrivate.js
@@ -3,8 +3,7 @@ import axios from "axios";
 const axiosPrivate = axios.create({});
 
 axiosPrivate.interceptors.request.use(
-  function (config) {
-    // Do something before request is sent
+  function (config) { 
     if (!config.headers.authorization) {
       config.headers.authorization = `Bearer ${localStorage.getItem(
         "accessToken"
@@ -13,24 +12,18 @@ axiosPrivate.interceptors.request.use(
     return config;
   },
   function (error) {
-    // Do something with request error
     return Promise.reject(error);
   }
 );
 
-// Add a response interceptor
+//  response interceptor
 axiosPrivate.interceptors.response.use(
   function (response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
     return response;
   },
   function (error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
     if (error.response.status === 403) {
       // refresh token
-      // send to the server
     }
     return Promise.reject(error);
   }
